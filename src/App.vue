2c79ef75c65a400ea9215403a676bccfacafc71b
Akane <865442122@qq.com>
2023-01-13 09:50:59 +0800

    样式-组件-调试

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <SideBar />
+    <router-view />
   </div>
 </template>
 
 <script>
+import SideBar from '@/components/SideBar.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SideBar
+  }
 }
 </script>
 
 <style>
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0
+}
+
+html,
+body,
 #app {
+  height: 100%;
+}
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  /* -webkit-font-smoothing 是webkit在自己的渲染引擎中增加的对字体抗锯齿的调整。这个属性仅在 Mac OS X/macOS 下生效。平滑像素级别的字体 */
   -webkit-font-smoothing: antialiased;
+  /*  Firefox的 -moz-osx-font-smoothing 这个属性仅在 Mac OS X / macOS 下生效.用灰度抗锯齿渲染文本 */
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
+}
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul,
+li {
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0px 0px 2px 0px #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+#app{
+  display: flex;
+  align-items: stretch;
 }
 </style>
